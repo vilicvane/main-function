6dc28a1988b1fb2ecc1e405eb6af29f15b7e10d2
vilicvane <vilicvane@live.com>
2023-10-03 19:13:33 +0800

    Update dependencies

--- src/library/program-error.ts
+++ src/library/program-error.ts
@@ -1,7 +1,10 @@
 import {ExtendableError} from 'extendable-error';
 
 export class ProgramError extends ExtendableError {
-  constructor(readonly code: number, message?: string) {
+  constructor(
+    readonly code: number,
+    message?: string,
+  ) {
     super(message);
   }
 }
