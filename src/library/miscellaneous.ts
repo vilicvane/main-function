97640aaa957d862b8d7b091e08392707bb9d7a67
vilicvane <vilicvane@live.com>
2023-10-03 19:17:30 +0800

    Add signal promise helper

--- src/library/miscellaneous.ts
+++ src/library/miscellaneous.ts
@@ -1,1 +1,13 @@
 export const BACKGROUND = new Promise<never>(() => {});
+
+export function SIGNAL(
+  signal: NodeJS.Signals | NodeJS.Signals[] = 'SIGINT',
+): Promise<void> {
+  const signals = Array.isArray(signal) ? signal : [signal];
+
+  return new Promise(resolve => {
+    for (const signal of signals) {
+      process.on(signal, () => resolve());
+    }
+  });
+}
